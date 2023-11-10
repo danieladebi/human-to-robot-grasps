b50b217b9d7f5b7b40d63cb78607291f5ce86b0b
Daniel Adebi <adebiike@outlook.com>
2023-11-10 07:12:08 -0600

    cleaned up mapping image to hand data fil

--- map_img_to_hand_data.py
+++ map_img_to_hand_data.py
@@ -3,6 +3,7 @@ import pickle
 import torch
 import torchvision
 import pickle
+import matplotlib.pyplot as plt
 
 from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import ToTensor
@@ -38,12 +39,14 @@ class HandPoseDataset(Dataset):
             
             self.img_ids.append(id)
 
-        self.handpose_ids.sort()
-        self.img_ids.sort()
+        ids = list(set(self.handpose_ids) & set(self.img_ids))
+
+        self.handpose_ids = sorted(ids)
+        self.img_ids = sorted(ids)
 
         assert(self.handpose_ids == self.img_ids)
-        for hp, img in zip(self.handpose_ids, self.img_ids):
-            assert(hp == img)
+        for hp_id, img_id in zip(self.handpose_ids, self.img_ids):
+            assert(hp_id == img_id)
         
 
     def __len__(self):
@@ -53,6 +56,7 @@ class HandPoseDataset(Dataset):
         img_id, hand_pose_id = self.img_ids[idx], self.handpose_ids[idx]
         img_filename = f"frame_{img_id}.jpg"
         img_filepath = os.path.join(self.img_dir, img_filename)
+        
         img = read_image(img_filepath)
 
         pkl_filename = f"frame_{hand_pose_id}_prediction_result.pkl"
@@ -70,51 +74,13 @@ class HandPoseDataset(Dataset):
             img = self.transform(img)
 
         return img, hand_pose
-        
 
-def load_data(mocap_dir, rendered_dir):
-    hand_poses = {}
-    imgs = {}
-
-    for pkl_filename in tqdm(os.listdir(mocap_dir)):
-        pkl_filepath = os.path.join(mocap_dir, pkl_filename)
-        id = re.findall(r'\d+', pkl_filename)[0]
-        with open(pkl_filepath, "rb") as f:
-            try:
-                hand_poses[id] = pickle.load(f)
-            except UnicodeDecodeError:
-                f.seek(0)
-                hand_poses[id] = pickle.load(f, encoding='latin1')
-
-
-    for img_filename in tqdm(os.listdir(rendered_dir)):
-        img_filepath = os.path.join(rendered_dir, img_filename)
-        img = torchvision.io.read_image(img_filepath)
-        id = re.findall(r'\d+', img_filename)[0]
-        imgs[id] = img
-            
-    return hand_poses, imgs
 
 if __name__ == "__main__":
-    mocap_dir = "/home/ida338/frankmocap/demo/results/mocap"
-    rendered_dir = "/home/ida338/frankmocap/demo/results/rendered"
+    mocap_dir = "/mnt/zhang-nas/ikadebi/handpose-data/mocap"
+    rendered_dir = "/mnt/zhang-nas/ikadebi/handpose-data/imgs"
 
     dataset = HandPoseDataset(mocap_dir, rendered_dir)
-    print(dataset[0])
-
-    # hand_pose_dict, img_dict = load_data(mocap_dir, rendered_dir)
-    # count = 0
-    # empty = 0
-    # for i in hand_pose_dict:
-    #     d = hand_pose_dict[i]['pred_output_list'][0]["right_hand"]
-    #     if d:
-    #         count += 1
-    #     else: 
-    #         empty += 1
-    # print(count, empty)
-    # print(hand_pose_dict['0000010000']["pred_output_list"][0]["right_hand"])
-
-    # data = [(img_dict[id], hand_pose_dict[id]["pred_output_list"][0]["right_hand"]) for id in hand_pose_dict.keys()] 
-
-    # Learn model that gets directly from image to hand joints
-    
+    plt.imshow(dataset[0][0].permute(1,2,0))
+    print(dataset[0][0].shape)
+    plt.savefig("sample.png")
